5567d46db749c8ee190639931d615ea4612da8ad
srini <srinivasgetid@gmail.com>
2016-05-25 14:35:22 +1000

    Wrote Response Object as generic

--- property_cc.go
+++ property_cc.go
@@ -36,17 +36,33 @@ type AllProperties struct {
 	Properties []Property `json:"properties"`
 }
 
-func (c *PropertyChainCode) responseObject(function string, message string, status string) ([]byte, error) {
-	resp_mssg := "\"message\":\"" + message + "\", "
-	resp_status := "\"status\":\"" + status
-	jsonResp := "{" + resp_mssg + resp_status + "}"
+type Response struct {
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
 
-	if status != "0" { // Status != 0 is error case
-		fmt.Printf(function + " ResponseJSON:\n", jsonResp)
-		return nil, errors.New(jsonResp)
-	} else { // Status == 0 is non error case
-		respAsBytes, _ := json.Marshal(jsonResp)
-		fmt.Printf(function + " ResponseJSON:\n", string(respAsBytes))
+func (c *PropertyChainCode) responseObject(function string, respMessage string, repStatus string) ([]byte, error) {
+	var res Response
+
+	/*message := "\"Message\":\"" + respMessage + "\", "
+	status := "\"Status\":" + string(repStatus)
+	jsonResp := "{" + message + status + "}"*/
+
+	res.Status = repStatus
+	res.Message = respMessage
+
+	/*err := json.Unmarshal([]byte(jsonResp), &res)
+	if err != nil {
+		fmt.Println("UnmarshalError Invalid JSON object on responseObject().!! ", err)
+	}*/
+	respAsBytes, _ := json.Marshal(res)
+	if repStatus != "0" {
+		// Status != 0 is error case
+		fmt.Printf(function + " function ERROR-ResponseJSON:\n", string(respAsBytes))
+		return nil, errors.New(string(respAsBytes))
+	} else {
+		// Status == 0 is non error case
+		fmt.Printf(function + " function ResponseJSON:\n", string(respAsBytes))
 		return respAsBytes, nil
 	}
 }
@@ -59,19 +75,18 @@ func (c *PropertyChainCode) Init(stub *shim.ChaincodeStub, function string, args
 // saveProperty - Writes to the ledger the Vehicle struct passed in a JSON format. Uses the shim file's
 //				  method 'PutState'.
 //==============================================================================================================================
-func (c *PropertyChainCode) saveProperty(stub *shim.ChaincodeStub, p Property) (bool, error) {
-	fmt.Println("*** Calling saveProperty() ****");
-
+func (c *PropertyChainCode) saveProperty(stub *shim.ChaincodeStub, p Property) ([]byte, error) {
 	bytes, err := json.Marshal(p)
 	if err != nil {
-		return false, errors.New("Error creating Property record")
+		fmt.Println("MarshalError while registering property", err)
+		c.responseObject("saveProperty", "MarshalError while registering property", "99")
 	}
 
 	err = stub.PutState(p.Folio_ID, bytes)
 
 	if err != nil {
-		fmt.Println("Error while registering property ")
-		return false, err
+		fmt.Println("Error while registering property", err)
+		c.responseObject("saveProperty", "Error while registering property", "99")
 	}
 
 	allPropAsBytes, err := stub.GetState("allProps")
@@ -80,28 +95,19 @@ func (c *PropertyChainCode) saveProperty(stub *shim.ChaincodeStub, p Property) (
 
 	//fmt.Printf("Query Response (GET ALL PROPS SIZE-BEFORE):\n", len(props.Properties))
 	props.Properties = append(props.Properties, p)
-
 	//fmt.Printf("Query Response (GET ALL PROPS SIZE-AFTER):\n", len(props.Properties))
 
 	jsonAsBytes, _ := json.Marshal(props)
 	err = stub.PutState("allProps", jsonAsBytes)
 	if err != nil {
-		fmt.Println("Error while PutState for allProps ")
-		return false, err
+		fmt.Println("Error while PutState for allProps ", err)
+		c.responseObject("saveProperty", "Error while PutState for allProps", "99")
 	}
 
-	/*newAllPropAsBytes, err := stub.GetState("allProps")
-	if err != nil {
-		return false, errors.New("Failed to get all Properties")
-	}
-	fmt.Printf("Query Response (GET ALL PROPS ***):\n", string(newAllPropAsBytes))*/
-
-	return true, nil
+	return c.responseObject("saveProperty", "Successfully property registered", "0")
 }
 
 func (c *PropertyChainCode) Register(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-	fmt.Println("*** Calling Register() ****");
-
 	// Variables to define the JSON
 	var p Property
 
@@ -109,22 +115,16 @@ func (c *PropertyChainCode) Register(stub *shim.ChaincodeStub, args []string) ([
 	folio_ID := "\"Folio_ID\":\"" + args[1] + "\", "
 	legalOwner := "\"LegalOwner\":\"" + args[2] + "\", "
 	beneficialOwner := "\"BeneficialOwner\":\"" + args[3] + "\", "
-	/*suburb := "\"Suburb\":\"" + args[3] + "\", "
-	state := "\"State\":\"" + args[4] + "\", "
-	postCode := "\"Postcode\":\"" + args[5] + "\", "*/
 	status := "\"Status\":0"
 
 	// Concatenates the variables to create the total JSON object
-	//property_json := "{" + folio_ID + legalOwner + address + suburb + state + postCode + status + "}"
 	property_json := "{" + folio_ID + legalOwner + beneficialOwner + address + status + "}"
 
 	//fmt.Println("*** Calling Register()- Property JSON:%s\n", property_json);
 
-	// matched = true if the v5cID passed fits format of two letters followed by seven digits
-	// regexp.Match("^[A-z][A-z][0-9]{7}", []byte(folio_ID))
-
+	// matched = true if the folio_ID passed fits format of "1/12345"
 	matched, err := regexp.Match("^[0-9]{1}[/.][0-9]{5}$", []byte(args[1]))
-	fmt.Println("*** Calling Register()- Property Is it matched:%s\n", matched);
+	//fmt.Println("*** Calling Register()- Property Is it matched:%s\n", matched);
 
 	if err != nil {
 		//return nil, errors.New("Invalid Folio Identifier")
@@ -141,6 +141,7 @@ func (c *PropertyChainCode) Register(stub *shim.ChaincodeStub, args []string) ([
 	err = json.Unmarshal([]byte(property_json), &p)
 
 	if err != nil {
+		fmt.Println("UnmarshalError Invalid JSON object..!! ", err)
 		//return nil, errors.New("Invalid JSON object")
 		return c.responseObject("Register", "Invalid JSON object..!!", "99")
 	}
@@ -153,29 +154,17 @@ func (c *PropertyChainCode) Register(stub *shim.ChaincodeStub, args []string) ([
 		return c.responseObject("Register", "Property already exists..!!", "99")
 	}
 
-	_, err = c.saveProperty(stub, p)
-
-	fmt.Println("*** Calling Register()- Property saved");
-	if err != nil {
-		fmt.Print(err);
-		return c.responseObject("Register", "Errors while saving the Property", "99")
-	}
-
-	return c.responseObject("Register", "Successfully property registered", "0")
+	return c.saveProperty(stub, p)
 }
 
 func (c *PropertyChainCode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	fmt.Println("invoke is running " + function)
-
 	// Handle different functions
 	if function == "init" {
 		return c.Init(stub, "init", args)
 	} else if function == "register" {
 		return c.Register(stub, args)
 	}
-	fmt.Println("invoke did not find func: " + function)
-
-	return nil, errors.New("Received unknown function invocation")
+	return c.responseObject("Query", "Unrecognized function:" + function, "99")
 }
 
 // ============================================================================================================================
@@ -185,13 +174,11 @@ func (c *PropertyChainCode) getProperties(stub *shim.ChaincodeStub, searchType s
 
 	var res AllProperties
 
-	fmt.Println("Start find getProperties..!")
 	fmt.Println("Looking for Property searchType:" + searchType + " searchValue:" + searchValue);
 
 	//get the AllProperties index
 	allPropAsBytes, err := stub.GetState("allProps")
 	if err != nil {
-		//return nil, errors.New("Failed to get all Properties")
 		return c.responseObject("Query", "Failed to get all Properties", "99")
 	}
 
@@ -221,7 +208,7 @@ func (c *PropertyChainCode) getProperties(stub *shim.ChaincodeStub, searchType s
 			}
 		default:
 			fmt.Printf("unrecognized property searchType..!!")
-			return c.responseObject("Query", "unrecognized property searchType..!!", "99")
+			return c.responseObject("Query", "Unrecognized property searchType..!!", "99")
 		}
 	}
 	resAsBytes, _ := json.Marshal(res)
@@ -235,8 +222,6 @@ func (c *PropertyChainCode) getProperties(stub *shim.ChaincodeStub, searchType s
 //  		initial arguments passed are passed on to the called function.
 //=================================================================================================================================
 func (c *PropertyChainCode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	fmt.Println("*** Calling Query() ****");
-
 	if len(args) != 2 {
 		return c.responseObject("Query", "Incorrect number of arguments passed", "99")
 	}
@@ -244,62 +229,7 @@ func (c *PropertyChainCode) Query(stub *shim.ChaincodeStub, function string, arg
 	if function == "search" {
 		return c.getProperties(stub, args[0], args[1])
 	}
-
-	/*if function != "query" {
-		return nil, errors.New("Invalid query function name. Expecting \"query\"")
-	}
-
-	var Folio_ID string // Entities
-	var err error
-
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
-	}
-
-	Folio_ID = args[0]
-
-	// Get the state from the ledger
-	valAsbytes, err := stub.GetState(Folio_ID)
-	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + Folio_ID + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	if valAsbytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + Folio_ID + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	jsonResp := "{\"Name\":\"" + Folio_ID + "\",\"Amount\":\"" + string(valAsbytes) + "\"}"
-	fmt.Printf("Query Response:%s\n", jsonResp)
-	//return jsonResp, nil
-
-	keysIter, err := stub.RangeQueryState("", "")
-	if err != nil {
-		return nil, fmt.Errorf("keys operation failed. Error accessing state: %s", err)
-	}
-	defer keysIter.Close()
-
-	var keys []string
-	for keysIter.HasNext() {
-		key, _, err := keysIter.Next()
-		if err != nil {
-			return nil, fmt.Errorf("keys operation failed. Error accessing state: %s", err)
-		}
-		keys = append(keys, key)
-	}
-
-	jsonKeys, err := json.Marshal(keys)
-	if err != nil {
-		return nil, fmt.Errorf("keys operation failed. Error marshaling JSON: %s", err)
-	}
-
-	fmt.Printf("Query Response-jsonKeys:%s\n", jsonKeys)
-	return jsonKeys, nil
-	*/
-
-	return nil, nil
-
+	return c.responseObject("Query", "Unrecognized function: " + function, "99")
 }
 
 //=================================================================================================================================
